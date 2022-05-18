ad511f4368b460171a2989d04819db95f0e14119
Eduardo González <alu0101319001@ull.edu.es>
2022-05-18 19:03:19 +0000

    Trying solve problems Heroku

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import express from 'express';
+import * as express from 'express';
+import './db/database';
 import { SongRouter } from './routers/song';
 import { ArtistRouter } from './routers/artist';
 import { PlaylistRouter } from './routers/playlist';
-import './database';
 
-const port = process.env.PORT || 8000;
 
-const app = express();
+const app = express.default();
 app.use(express.json());
-
 app.use(SongRouter);
 app.use(ArtistRouter);
 app.use(PlaylistRouter);
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log('Server has started at port ', port);
