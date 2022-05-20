64722c72bce7c33c5db184344c7cf767cd60bd24
Eduardo González <alu0101319001@ull.edu.es>
2022-05-20 18:32:14 +0000

    Resolving models validators v1

--- src/models/artist.ts
+++ src/models/artist.ts
@@ -15,7 +15,7 @@ const ArtistSchema = new Schema<ArtistDocumentInterface>({
         required: true,
         trim: true,
         validate: (value: string) => {
-            if (!value.match(/^[A-Za-z0-0\s]+$/g)) {
+            if (!value.match(/^[A-Za-z0-0-'-,\s]+$/g)) {
                 throw new Error('The artist name must be a combination of letters, numbers or spaces.');
             }            
         }
@@ -35,7 +35,7 @@ const ArtistSchema = new Schema<ArtistDocumentInterface>({
         required: true,
         trim: true,
         validate: (value: string) => {
-            if (!value.match(/^[A-Za-z0-0\s]+$/g)) {
+            if (!value.match(/^[A-Za-z0-0-'-,\s]+$/g)) {
                 throw new Error('The song name must be a combination of letters, numbers or spaces.');
             }            
         }
