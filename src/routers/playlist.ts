d9fb4ed76a2cbd7472b403146754d3a73605a52c
Eduardo González <alu0101319001@ull.edu.es>
2022-05-21 14:06:51 +0000

    Resolving routers v1

--- src/routers/playlist.ts
+++ src/routers/playlist.ts
@@ -7,40 +7,126 @@ export const PlaylistRouter = express.Router();
 PlaylistRouter.post('/playlist', (req, res) => {
     const playlist = new Playlist(req.body);
     playlist.save().then((data) => {
-        res.json(data);
-    }).catch((error) => { 
-        res.json({ message: error });
+        return res.status(201).send(data);
+    }).catch((err) => { 
+        return res.status(400).send(err);
     });
 });
 
-// get playlist
+// get playlist by name
+PlaylistRouter.get('/playlist', (req, res) => {
+    const filter = req.query.name?{name: req.query.name.toString()}:{};
+    Playlist.find(filter).then((data) => {
+        if (data.length !== 0) {
+            return res.send(data);
+        } else {
+            return res.status(404).send();
+        }
+    }).catch((err) => {
+        return res.status(500).send(err);
+    });
+});
+
+// get playlist by id
 PlaylistRouter.get('/playlist/:id', (req, res) => {
-    // const id = req.query.id
-    const { id } = req.params;
-    Playlist.findById(id).then((data) => {
-        res.json(data);
-    }).catch((error) => {
-        res.json({ message: error });
+    Playlist.findById(req.params.id).then((data: any) => {
+        if (!data) {
+            return res.status(404).send();
+        } else {
+            return res.send(data);
+        }
+    }).catch((err) => {
+        return res.status(500).send(err);
     });
 });
 
-// delete playlist
+// delete playlist by name
+PlaylistRouter.delete('/playlist', (req, res) => {
+    if (!req.query.name) {
+        return res.status(400).send({
+            error: 'A name must be provided',
+        });
+    } else {
+        Playlist.findOneAndDelete({name: req.query.name.toString()}).then((data: any) => {
+            if (!data) {
+                return res.status(404).send();
+            } else {
+                return res.send(data);
+            }
+        }).catch((err: any) => {
+            return res.status(400).send(err);
+        });
+    }
+});
+// delete playlist by id
 PlaylistRouter.delete('/playlist/:id', (req, res) => {
-    const { id } = req.params;
-    Playlist.deleteOne({ _id: id }).then((data) => {
-        res.json(data);
-    }).catch((error) => {
-        res.json({ message: error });
+    Playlist.findByIdAndDelete(req.params.id).then((data: any) => {
+        if (!data) {
+            return res.status(404).send();
+        } else {
+            return res.send(data);
+        }
+    }).catch((err: any) => {
+        return res.status(400).send(err);
     });
 });
 
-// update playlist
+// update playlist by name
+PlaylistRouter.patch('/playlist', (req, res) => {
+    if (!req.query.name) {
+      return res.status(400).send({
+        error: 'A name must be provided'
+      });
+    } else {
+      const allowedUpdates = ['name', 'songs', 'duration', 'genres'];
+      const actualUpdates = Object.keys(req.body);
+      const isValidUpdate =
+        actualUpdates.every((update) => allowedUpdates.includes(update));
+  
+      if (!isValidUpdate) {
+        return res.status(400).send({
+          error: 'Update is not permitted',
+        });
+      } else {
+        Playlist.findOneAndUpdate({name: req.query.name.toString()}, req.body, {
+          new: true,
+          runValidators: true,
+        }).then((data) => {
+          if (!data) {
+            return res.status(404).send();
+          } else {
+            return res.send(data);
+          }
+        }).catch((err) => {
+          return res.status(400).send(err);
+        });
+      }
+    }
+});
+
+// update playlist by id
 PlaylistRouter.put('/playlist/:id', (req, res) => {
-    const { id } = req.params;
-    const { name, songs, duration, genres } = req.body;
-    Playlist.updateOne({ _id: id }, { $set: {name, songs, duration, genres} }).then((data) => {
-        res.json(data);
-    }).catch((error) => {
-        res.json({ message: error });
-    });
+    const allowedUpdates = ['name', 'songs', 'duration', 'genres'];
+    const actualUpdates = Object.keys(req.body);
+    const isValidUpdate =
+      actualUpdates.every((update) => allowedUpdates.includes(update));
+
+    if (!isValidUpdate) {
+       return res.status(400).send({
+           error: 'Update is not permitted',
+       });
+    } else {
+        Playlist.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true,
+        }).then((data) => {
+            if (!data) {
+                return res.status(404).send();
+            } else {
+                return res.send(data);
+            }
+        }).catch((err) => {
+            return res.status(400).send(err);
+        });
+    }
 });
--- src/routers/song.ts
+++ src/routers/song.ts
@@ -7,40 +7,127 @@ export const SongRouter = express.Router();
 SongRouter.post('/song', (req, res) => {
     const song = new Song(req.body);
     song.save().then((data) => {
-        res.json(data);
-    }).catch((error) => { 
-        res.json({ message: error });
+        return res.status(201).send(data);
+    }).catch((err) => { 
+        return res.status(400).send(err);
     });
 });
 
-// get song
+// get song by name
+SongRouter.get('/song', (req, res) => {
+    const filter = req.query.name?{name: req.query.name.toString()}:{};
+    Song.find(filter).then((data) => {
+        if (data.length !== 0) {
+            return res.send(data);
+        } else {
+            return res.status(404).send();
+        }
+    }).catch((err) => {
+        return res.status(500).send(err);
+    });
+});
+
+// get song by id
 SongRouter.get('/song/:id', (req, res) => {
-    // const id = req.query.id
-    const { id } = req.params;
-    Song.findById(id).then((data) => {
-        res.json(data);
-    }).catch((error) => {
-        res.json({ message: error });
+    Song.findById(req.params.id).then((data: any) => {
+        if (!data) {
+            return res.status(404).send();
+        } else {
+            return res.send(data);
+        }
+    }).catch((err) => {
+        return res.status(500).send(err);
     });
 });
 
-// delete song
+// delete song by name
+SongRouter.delete('/song', (req, res) => {
+    if (!req.query.name) {
+        return res.status(400).send({
+            error: 'A name must be provided',
+        });
+    } else {
+        Song.findOneAndDelete({name: req.query.name.toString()}).then((data: any) => {
+            if (!data) {
+                return res.status(404).send();
+            } else {
+                return res.send(data);
+            }
+        }).catch((err: any) => {
+            return res.status(400).send(err);
+        });
+    }
+});
+
+// delete song by id
 SongRouter.delete('/song/:id', (req, res) => {
-    const { id } = req.params;
-    Song.deleteOne({ _id: id }).then((data) => {
-        res.json(data);
-    }).catch((error) => {
-        res.json({ message: error });
+    Song.findByIdAndDelete(req.params.id).then((data: any) => {
+        if (!data) {
+            return res.status(404).send();
+        } else {
+            return res.send(data);
+        }
+    }).catch((err: any) => {
+        return res.status(400).send(err);
     });
 });
 
-// update song
+// update song by name
+SongRouter.patch('/song', (req, res) => {
+    if (!req.query.name) {
+      return res.status(400).send({
+        error: 'A name must be provided'
+      });
+    } else {
+      const allowedUpdates = ['name', 'author', 'duration', 'genres', 'single', 'numberReproductions'];
+      const actualUpdates = Object.keys(req.body);
+      const isValidUpdate =
+        actualUpdates.every((update) => allowedUpdates.includes(update));
+  
+      if (!isValidUpdate) {
+        return res.status(400).send({
+          error: 'Update is not permitted',
+        });
+      } else {
+        Song.findOneAndUpdate({name: req.query.name.toString()}, req.body, {
+          new: true,
+          runValidators: true,
+        }).then((data) => {
+          if (!data) {
+            return res.status(404).send();
+          } else {
+            return res.send(data);
+          }
+        }).catch((err) => {
+          return res.status(400).send(err);
+        });
+      }
+    }
+});
+
+// update song by id
 SongRouter.put('/song/:id', (req, res) => {
-    const { id } = req.params;
-    const { name, author, duration, genres, single, reporductions } = req.body;
-    Song.updateOne({ _id: id }, { $set: {name, author, duration, genres, single, reporductions} }).then((data) => {
-        res.json(data);
-    }).catch((error) => {
-        res.json({ message: error });
-    });
+    const allowedUpdates = ['name', 'author', 'duration', 'genres', 'single', 'numberReproductions'];
+    const actualUpdates = Object.keys(req.body);
+    const isValidUpdate =
+      actualUpdates.every((update) => allowedUpdates.includes(update));
+
+    if (!isValidUpdate) {
+       return res.status(400).send({
+           error: 'Update is not permitted',
+       });
+    } else {
+        Song.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true,
+        }).then((data) => {
+            if (!data) {
+                return res.status(404).send();
+            } else {
+                return res.send(data);
+            }
+        }).catch((err) => {
+            return res.status(400).send(err);
+        });
+    }
 });
